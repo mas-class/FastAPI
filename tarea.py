d62e9e7282ec92ccd5632f016615639afd2d49d3
mas-class <92550473+mas-class@users.noreply.github.com>
2021-11-30 19:26:50 -0600

    Add files via upload

--- tarea.py
+++ tarea.py
@@ -17,12 +17,28 @@ def Suma(a: int, b: int):
     result = {'a': a, 'b': b, 'a+b': c}
     return result
 
+# Funcion para Sumar
+@app.post("/Suma")
+def Suma(a: int, b: int):
+    c = a + b
+    result = {'a': a, 'b': b, 'a+b': c}
+    return result
+
+
 # Funcion para Restar
 @app.get("/Resta")
 def Resta(a: int, b: int):
     c = a - b
     result = {'a': a, 'b': b, 'a-b': c}
     return result
+
+# Funcion para Restar
+@app.post("/Resta")
+def Resta(a: int, b: int):
+    c = a - b
+    result = {'a': a, 'b': b, 'a-b': c}
+    return result
+
 
 # Funcion para Multiplicar
 @app.get("/Multiplicacion")
@@ -31,12 +47,31 @@ def Multiplicacion(a: int, b: int):
     result = {'a': a, 'b': b, 'a*b': c}
     return result
 
+# Funcion para Multiplicar
+@app.post("/Multiplicacion")
+def Multiplicacion(a: int, b: int):
+    c = a * b
+    result = {'a': a, 'b': b, 'a*b': c}
+    return result
+
+
 # Funcion para Dividir
 @app.get("/Division")
 def Division(a: int, b: int):
     c = a / b
     result = {'a': a, 'b': b, 'a/b': c}
     return result
+
+# Funcion para Dividir
+@app.post("/Division")
+def Division(a: int, b: int):
+    c = a / b
+    result = {'a': a, 'b': b, 'a/b': c}
+    return result
+
+
+
+
 
 
 
