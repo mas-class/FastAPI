f97166f12f61846f99784ba2dfdbe74512945bff
mas-class <92550473+mas-class@users.noreply.github.com>
2021-11-30 19:16:02 -0600

    Add files via upload

---
+++ tarea.py
@@ -0,0 +1,69 @@
+# Tarea Operaciones Matematicas
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+# uvicorn (nombre del archivo):app --reload
+
+# Crear clases
+class Item(BaseModel):
+    a: int = None
+    b: int = None
+
+# Funcion para Sumar
+@app.get("/Suma")
+def Suma(a: int, b: int):
+    c = a + b
+    result = {'a': a, 'b': b, 'a+b': c}
+    return result
+
+# Funcion para Restar
+@app.get("/Resta")
+def Resta(a: int, b: int):
+    c = a - b
+    result = {'a': a, 'b': b, 'a-b': c}
+    return result
+
+# Funcion para Multiplicar
+@app.get("/Multiplicacion")
+def Multiplicacion(a: int, b: int):
+    c = a * b
+    result = {'a': a, 'b': b, 'a*b': c}
+    return result
+
+# Funcion para Dividir
+@app.get("/Division")
+def Division(a: int, b: int):
+    c = a / b
+    result = {'a': a, 'b': b, 'a/b': c}
+    return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
